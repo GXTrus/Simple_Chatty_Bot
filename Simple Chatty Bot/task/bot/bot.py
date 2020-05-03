0c0223da3c10653d5cefd27d4e333ffd96c6202d
StrangeGreyCat <gxtrus@gmail.com>
2020-05-03 09:37:12 +0300

    Stage #2: What's my name

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,14 +1,21 @@
-# write your code here
 class SimpleChattyBot:
 
     def __init__(self):
         self.bot_name = "Aid"
         self.birth_year = 2020
+        self.show_info()
+        self.ask_name()
+        self.show_your_name()
 
     def show_info(self):
         print(f"Hello! My name is {self.bot_name}.")
         print(f"I was created in {self.birth_year}.")
 
+    def ask_name(self):
+        self.your_name = input("Please, remind me your name.\n")
+
+    def show_your_name(self):
+        print(f"What a great name you have, {self.your_name}!")
+
 
 bot = SimpleChattyBot()
-bot.show_info()
