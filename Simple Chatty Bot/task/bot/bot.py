3934fd8706525d6769cb4694ede4f07330b3f786
StrangeGreyCat <gxtrus@gmail.com>
2020-05-03 09:52:18 +0300

    Stage #3: How old are you

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -4,18 +4,22 @@ class SimpleChattyBot:
         self.bot_name = "Aid"
         self.birth_year = 2020
         self.show_info()
-        self.ask_name()
-        self.show_your_name()
+        self.ask_show_name()
+        self.ask_show_age()
 
     def show_info(self):
         print(f"Hello! My name is {self.bot_name}.")
         print(f"I was created in {self.birth_year}.")
 
-    def ask_name(self):
+    def ask_show_name(self):
         self.your_name = input("Please, remind me your name.\n")
-
-    def show_your_name(self):
         print(f"What a great name you have, {self.your_name}!")
+
+    def ask_show_age(self):
+        print("Let me guess your age.")
+        print("Say me remainders of dividing your age by 3, 5 and 7.")
+        self.your_age = (int(input()) * 70 + int(input()) * 21 + int(input()) * 15) % 105
+        print(f"Your age is {self.your_age}; that's a good time to start programming!")
 
 
 bot = SimpleChattyBot()
