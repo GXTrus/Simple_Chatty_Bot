740c44a653cb52aefbcc7f91a59491635dba48f9
StrangeGreyCat <gxtrus@gmail.com>
2020-05-03 09:23:19 +0300

    Initial commit

---
+++ Simple Chatty Bot/task/bot/bot.py
@@ -0,0 +1,13 @@
+# write your code here
+class SimpleChattyBot:
+
+    def __init__(self):
+    bot_name = "Aid"
+    birth_year = 2020
+
+    def show_info(self):
+        print(f"Hello! My name is {self.bot_name}.")
+        print(f"I was created in {self.birth_year}.")
+
+bot = SimpleChattyBot
+bot.show_info()
