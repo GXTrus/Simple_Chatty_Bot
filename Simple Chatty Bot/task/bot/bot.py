a8b9e3a936257db4ddd5b898d4fcac07bb8b06cc
StrangeGreyCat <gxtrus@gmail.com>
2020-05-03 09:59:00 +0300

    Stage #4: Let’s count!

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -6,6 +6,7 @@ class SimpleChattyBot:
         self.show_info()
         self.ask_show_name()
         self.ask_show_age()
+        self.make_counter()
 
     def show_info(self):
         print(f"Hello! My name is {self.bot_name}.")
@@ -20,6 +21,13 @@ class SimpleChattyBot:
         print("Say me remainders of dividing your age by 3, 5 and 7.")
         self.your_age = (int(input()) * 70 + int(input()) * 21 + int(input()) * 15) % 105
         print(f"Your age is {self.your_age}; that's a good time to start programming!")
+
+    def make_counter(self):
+        print('Now I will prove to you that I can count to any number you want.')
+        for i in range(int(input()) + 1):
+            print(i, '!')
+
+        print('Completed, have a nice day!')
 
 
 bot = SimpleChattyBot()
